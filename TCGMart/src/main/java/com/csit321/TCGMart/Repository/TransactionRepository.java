d5b7ff6743afe7b057556ce8ef479b0d9732fa40
John Carl Sabejon <143713929+NifaNii@users.noreply.github.com>
2023-12-16 00:27:50 +0800

    7.2
    -added findByCardBuyer

--- TCGMart/src/main/java/com/csit321/TCGMart/Repository/TransactionRepository.java
+++ TCGMart/src/main/java/com/csit321/TCGMart/Repository/TransactionRepository.java
@@ -1,5 +1,7 @@
 package com.csit321.TCGMart.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.csit321.TCGMart.Entity.TransactionEntity;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long>{
-
+	List<TransactionEntity> findByCardBuyer(int cardBuyer);
 }
